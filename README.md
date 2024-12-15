9289e68e6777a39fefcff3e14981e73abcbbf1a8
Alosh Denny <97750960+aloshdenny@users.noreply.github.com>
2024-12-15 11:49:44 +0530

    finally!

---
+++ README.md
@@ -0,0 +1,96 @@
+# mygpt
+An easily-trained baby GPT that can stand in for the real thing. Based on Andrej Karpathy's makemore, but set up to mimic a llama-cpp server.
+
+The main points of differentiation are:
+ - my version is token-based (tiktoken)
+ - code to load up multiple text files as a training set
+ - extra inference parameters, such as top_k, and the supression of tokens which you do not want to see (ie glitch tokens or annoyingly repeated tokens).
+
+So you can train the default tiny 15M parameter model, and use that in your projects instead of ChatGPT.
+
+
+This is not production-ready; it's a toy implementation for educational purposes.
+
+## Setup
+
+pip install -r requirements.txt
+
+Add as many text files as you want to the "data" folder as a trianing set.
+
+## Using it
+
+It is not very configurable at the moment -tweak the code in main.py to get it to do what you want.
+
+### Training
+
+Uncomment "train()" in main.py. It will save checkpoints of the model parameters into the "models" folder.
+
+### Inference / text generation
+
+Once you have trained the model, comment "train()" and uncomment "inference()". Setup whatever prompt you want. Then run the script to see the generated text appear.
+
+
+## Example output
+
+These are some sample responses from a model trained on a dozen old Encyclopedia Brittanica volumes for a couple of hours on an NVidia 4GB GPU, then fine-tuned on 120 dad-jokes from the internet.
+
+```
+Q: What is a dog?
+A: To get a frog.
+
+Q: Why did the chicken cross the road?
+A: Because it was Sunday.
+```
+
+With "Q: " as a prompt, it will make its own "jokes":
+
+```
+Q: How do you cross a race with no cold birds?
+A: Because they did the toothache entirely.
+
+Q: Why did a figureur hit a like?
+A: Because a joke.
+````
+
+Pure comic genius!
+
+The prompt format is:
+```
+Q: {user question}
+```
+
+Here is part of the fine-tuning set (real dad jokes from the internet - not what was generated):
+Q: What do you call a fake noodle? A: An impasta
+Q: How do you organise a space party? A: You planet!
+
+
+## Improving the performance
+
+This code is an educational exercise; it has a self-attention head built from first principles.
+
+You can get a great memory + speed saving by replacing the self-attention head with a prebuilt module from pytorch. Replace the old Block class with this code:
+
+```
+class Block(nn.Module):
+    """ Intersperse communication (attention) and computation """
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        self.sa = nn.MultiheadAttention(n_embd, n_head, dropout=dropout)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        l1_results = self.ln1(x)
+        sa_results, _ = self.sa(l1_results, l1_results, l1_results, need_weights=False)
+        x = x + sa_results  # note the x+ is a residual connection to help with optimisation
+        x = x + self.ffwd(self.ln2(x))
+        return x
+```
+
+I got a ~30% memory reduction.
+
+This is probably because many separate steps in our layers can be mathematically simplified when the layers are combined into a single module. 
+Plus, pytorch is no doubt much better optimised than our naive python implementation.
+
---
+++ main.py
@@ -0,0 +1,183 @@
+import asyncio
+import json
+import torch
+import tiktoken
+from model import GPTModel, get_batch, device
+from contextlib import asynccontextmanager
+
+enc = tiktoken.get_encoding("p50k_base")
+m = None
+
+
+@asynccontextmanager
+async def lifespan():
+    global enc, m
+    # Load the ML model
+    model = GPTModel(enc.n_vocab)
+    m = model.to(device)
+    print("Loading model...")
+    m.load_state_dict(torch.load("models/mine.pt"))
+
+    yield
+    # Clean up the ML models and release the resources
+
+
+class CreateCompletionRequest:
+    def __init__(self, prompt, stream=False, stop=None, max_tokens=100, temperature=1.0):
+        self.prompt = prompt
+        self.stream = stream
+        self.stop = stop or []
+        self.max_tokens = max_tokens
+        self.temperature = temperature
+
+
+def on_generated(token):
+    return
+
+
+def completions(body: CreateCompletionRequest):
+    """ Mimic the Llama-CPP server's completion functionality """
+    global enc, m
+
+    stops = [{"choices": [{"text": enc.encode(stop)[0]}]} for stop in body.stop]
+
+    prompt_tokens = enc.encode(body.prompt)
+    idx = torch.tensor([prompt_tokens], dtype=torch.long, device=device)
+    ret = m.generate(idx, max_new_tokens=body.max_tokens, stop_tokens=stops, generated=on_generated)
+
+    tokens = ret.tolist()[0]
+
+    if body.stream:
+        # This is a terrible hack to do response streaming: just wait until it is done and then dribble out the tokens.
+        # Couldn't be bothered converting my model to async.
+        chunks = [json.dumps({"choices": [{"text": enc.decode([token])}]}) for token in tokens]
+
+        async def event_generator():
+            while True:
+                if len(chunks) == 0:
+                    break
+                yield chunks.pop(0)
+                await asyncio.sleep(0.01)
+
+        return event_generator()  # Return generator function for streaming response
+    else:
+        return {"resp": enc.decode(tokens)}
+
+
+def text_from_path(path):
+    """ Load all the text files found at this path into one huge lump of text and return it. """
+    import glob
+    import os
+
+    text = ""
+    print(f"Searching for text files in: {os.path.abspath(path)}")
+    files = glob.glob(path+"*.txt")
+
+    print(f"Found {len(files)} text files:")
+    for filename in files:
+        print(filename)
+        try:
+            with open(filename, 'r', encoding='utf-8') as f:
+                lines = f.readlines()
+                for line in lines:
+                    text += line
+        except UnicodeDecodeError:
+            print(f"Warning: Could not read {filename} with UTF-8 encoding. Trying alternative encodings.")
+            try:
+                with open(filename, 'r', encoding='latin-1') as f:
+                    lines = f.readlines()
+                    for line in lines:
+                        text += line
+            except Exception as e:
+                print(f"Error reading {filename}: {e}")
+
+    print(f"Total text length: {len(text)} characters")
+    return text
+
+
+def text_from_file():
+    """ Open a single file and return its contents. """
+    text = ""
+    with open('input.txt') as f:
+        lines = f.readlines()
+        for line in lines:
+            text += line
+    return text
+
+
+def train():
+    """ Train the model and save it at regular checkpoints. """
+    global enc, m
+
+    # Change this path to where your training data is, as a folder full of .txt files
+    text = text_from_path("./")
+
+    tokens = torch.tensor(enc.encode(text), dtype=torch.long)
+
+    print(f"Vocab size {enc.n_vocab}")
+    print(f"Training data size {len(tokens)}")
+
+    model = GPTModel(enc.n_vocab)
+    m = model.to(device)
+    print(sum(p.numel() for p in m.parameters()) / 1e6, "M parameters")
+
+    # Try to load the old model so we can continue training it from where we left off.
+    try:
+        m.load_state_dict(torch.load("models/mine.pt"))
+    except FileNotFoundError:
+        print("Model file not found, starting new training...")
+    print("Loaded model. Beginning training...")
+
+    optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+
+    epoch = 300
+    for checkpoints in range(10):
+        loss_total = 0
+        for steps in range(epoch):
+            xb, yb = get_batch(tokens)
+            logits, loss = m(xb, yb)
+            optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            optimizer.step()
+            loss_total += loss.item()
+
+        print(loss_total/epoch)
+
+        torch.save(model.state_dict(), "models/mine.pt")
+    print("Done with training!")
+
+
+def inference():
+    """ Generate a stream of text from a starting prompt. """
+    global m, enc
+
+    model = GPTModel(enc.n_vocab)
+    m = model.to(device)
+
+    # This is the model that was trained previously. The hyperparameters in model.py must match exactly to when it was trained, or there'll be an error.
+    print("Loading model...")
+    m.load_state_dict(torch.load("models/mine.pt"))
+
+    def on_generated(token):
+        #print(f"{enc.decode([token])}({token.item()})", end="")
+        print(enc.decode([token]), end="")
+
+    stops = [enc.encode(".")[0], enc.encode("?")[0], enc.encode("!")[0]]
+    prompt = "Q: Why did the chicken cross the road?\n"
+    print(f"{prompt}", end="")
+    prompt_tokens = enc.encode(prompt)
+    # supress = torch.tensor([0, 930], dtype=torch.long, device=device) # the and and: 290, 262 as a testy=
+    supress = []
+    idx = torch.tensor([prompt_tokens], dtype=torch.long, device=device)
+    m.generate(idx, max_new_tokens=100, stop_tokens=None, generated=on_generated, top_k=16, sample=True, supress_tokens=supress)
+    print("\n")
+
+
+if __name__ == '__main__':
+    print(f"Using device {device}")
+
+    # Uncomment this to train the model
+    # train()
+
+    # Uncomment this for inference testing
+    inference()
